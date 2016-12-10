efd5503f100b5af70197cf1987cab9d606e774c3
Anthony Ardito <anthony.ardito@gmail.com>
2016-12-10 12:18:51 -0500

    package.json additions

--- src/utils/storeShape.js
+++
@@ -1,8 +0,0 @@
-import { PropTypes } from 'react';
-
-export default PropTypes.shape({
-  subscribe: PropTypes.func.isRequired,
-  dispatch: PropTypes.func.iRequired,
-  getState: PropTypes.func.isRequired,
-});
-
